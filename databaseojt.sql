eb125f9c8b953d53a9467c3d2ae13c86bad08918
DarrenS <darren_sison2026@yahoo.com>
2018-05-14 19:38:44 +0800

    Updated the database

---
+++ databaseojt.sql
@@ -0,0 +1,187 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 14, 2018 at 01:37 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `databaseojt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attend`
+--
+
+CREATE TABLE `attend` (
+  `attend_id` bigint(20) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `schedule_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `attend`
+--
+
+INSERT INTO `attend` (`attend_id`, `user_id`, `schedule_id`) VALUES
+(12, 6, 2),
+(13, 6, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `company_id` int(11) NOT NULL,
+  `company_name` varchar(255) NOT NULL,
+  `contact_name` varchar(100) NOT NULL,
+  `phone` int(15) NOT NULL,
+  `landline` int(9) NOT NULL,
+  `archive` enum('0','1') NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `company`
+--
+
+INSERT INTO `company` (`company_id`, `company_name`, `contact_name`, `phone`, `landline`, `archive`) VALUES
+(1, 'Nokia', '', 0, 0, '1'),
+(2, 'Any', '', 0, 0, '0'),
+(3, '', 'Mr. Brad Pitt', 112331000, 0, '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `list`
+--
+
+CREATE TABLE `list` (
+  `list_id` int(11) NOT NULL,
+  `schedule_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `schedule`
+--
+
+CREATE TABLE `schedule` (
+  `schedule_id` int(11) NOT NULL,
+  `company_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `start_time` text NOT NULL,
+  `end_time` text NOT NULL,
+  `room` varchar(255) NOT NULL,
+  `archive` enum('0','1') DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `schedule`
+--
+
+INSERT INTO `schedule` (`schedule_id`, `company_id`, `date`, `start_time`, `end_time`, `room`, `archive`) VALUES
+(1, 1, '2018-04-15', '00:00:00', '23:00:00', 'test1', '0'),
+(2, 1, '2018-04-14', '00:00:00', '20:00:00', '', '1'),
+(3, 2, '2018-04-15', '00:00:00', '15:00:00', '', '1'),
+(4, 1, '2018-05-05', '8:00 AM', '9:00 AM', 'D123', '1'),
+(5, 2, '2018-05-05', '8:00 AM', '9:00 AM', 'D333', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `type` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `email`, `password`, `type`, `firstname`, `lastname`) VALUES
+(1, 'admin', 'admin', 0, 'admin fname', 'admin lname'),
+(2, 'facilitator', 'facilitator', 1, 'facilitator name', 'facilitator last'),
+(3, 'noms@gmail.com', '12345', 0, 'Nomar', 'Flores'),
+(4, 'nomsy@gmail.com', '12345', 0, 'Nomar', 'Flores'),
+(5, 'nomsy@gmail.com', '0000', 2, 'Nomar', 'Flores'),
+(6, 'armanflores@ymail.com', '12345', 2, 'armando', 'flores');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attend`
+--
+ALTER TABLE `attend`
+  ADD PRIMARY KEY (`attend_id`);
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`company_id`);
+
+--
+-- Indexes for table `schedule`
+--
+ALTER TABLE `schedule`
+  ADD PRIMARY KEY (`schedule_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attend`
+--
+ALTER TABLE `attend`
+  MODIFY `attend_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT for table `company`
+--
+ALTER TABLE `company`
+  MODIFY `company_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `schedule`
+--
+ALTER TABLE `schedule`
+  MODIFY `schedule_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
